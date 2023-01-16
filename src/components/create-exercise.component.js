2df6105131e9ff3797838a6a62898da5d23793ab
Mustafa <mustafa-naveed@hotmail.com>
2023-01-17 02:27:34 +0500

    frontend initialization

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class CreateExercise extends Component {
+    render () {
+  return (
+    <div >
+    <p> hell1</p>
+    </div>
+  );
+}
+}
---
+++ src/components/exercise-list.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class ExerciseList extends Component {
+    render () {
+  return (
+    <div >
+     <p> hell3</p>
+    </div>
+  );
+}
+}
