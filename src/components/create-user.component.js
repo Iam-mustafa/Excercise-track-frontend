aeb73489ebf4d26005fc8f7bd96992df50186928
Mustafa <mustafa-naveed@hotmail.com>
2023-01-31 23:59:11 +0500

    refactor

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,32 +1,37 @@
 import React, {Component} from "react";
 import axios from 'axios';
-export default class CreateUser extends Component {
-  constructor(props) {
-    super(props);
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.state = {
-      username: ''
-    };
+
+const CreateUser = () =>{
+  // constructor(props) {
+  //   super(props);
+  //   this.onChangeUsername = this.onChangeUsername.bind(this);
+  //   this.onSubmit = this.onSubmit.bind(this);
+  //   this.state = {
+  //     username: ''
+  //   };
+  // }
+  const onChangeUsername = (event) => {
+    setSearchDate({ ...searchDate, [event.target.name]: event.target.value })
+    console.log(searchDate)
   }
-  onChangeUsername(e) {
-    this.setState({
-      username: e.target.value
-    });
-  }
-  onSubmit(e) {
-    e.preventDefault();
-    const newUser = {
-      username: this.state.username,
-    };
-    console.log(newUser);
-    axios.post('http://localhost:5000/users/add', newUser)
-    .then(res => console.log(res.data));
-    this.setState({
-      username: ''
-    })
-  }
-    render () {
+  // onChangeUsername(e) {
+  //   this.setState({
+  //     username: e.target.value
+  //   });
+  // }
+  // onSubmit(e) {
+  //   e.preventDefault();
+  //   const newUser = {
+  //     username: this.state.username,
+  //   };
+  //   console.log(newUser);
+  //   axios.post('http://localhost:5000/users/add', newUser)
+  //   .then(res => console.log(res.data));
+  //   this.setState({
+  //     username: ''
+  //   })
+  // }
+  //   render () {
   return (
   <div>
   <h3>Create New User</h3>
@@ -47,4 +52,4 @@ export default class CreateUser extends Component {
 </div>
   );
 }
-}
+export default CreateUser;
